d59d67fa47902ea4e699b6492bb0b6f4c11181a5
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-01-22 20:27:08 -0600

    finished Round1 picking up coins

--- Round1/Round1.ino
+++ Round1/Round1.ino
@@ -23,7 +23,7 @@
 #define pinIRMatrix3
 #define pinIRMatrix4
 #define pinIRMatrix5
-#define colorServoPin
+#define colorServoPin 
 
 void setup() {
   // put your setup code here, to run once:
@@ -32,6 +32,7 @@ void setup() {
   Encoder leftEnc = new Encoder();
   Encoder rightEnc = new Encoder();
   Gyro gyro = new Gyro();
+  DigitalDevice mDetector = new DigitalDevice();
   IRMatrix matrix = new IRMatrix(pinIRMatrix1, pinIRMatrix2, pinIRMatrix3, pinIRMatrix4, pinIRMatrix5);
   
   leftMot.begin(pinLeftMot1, pinLeftMot2, 1);
@@ -39,13 +40,14 @@ void setup() {
   leftEnc.begin(pinLeftEnc1, pinLeftEnc2, 1);
   rightEnc.begin(pinRightEnc1, pinRightEnc2 1);
   gyro.begin(pinGyro1, pinGyro2);                                                              //make sure gyro is correct
+  mDetector.initialize(pinMetDet);
   matrix.begin(pinIRMatrix1, pinIRMatrix2, pinIRMatrix3, pinIRMatrix4, pinIRMatrix5);
   
   Drivetrain drivetrain = new Drivetrain();
-  drivetrain.begin(leftMot, rightMot, leftEnc, rightEnc, gyro, matrix);
+  drivetrain.begin(leftMot, rightMot, leftEnc, rightEnc, gyro, matrix, mDetector);
   
   Encoder tEncoder = new Encoder();
-  DigitalDevice mDetector = new DigitalDevice();
+  
   DigitalDevice lSwitch = new DigitalDevice();
   Electromagnet eMagnet = new Electromagnet();
   Servo tServo = new Servo();                                    //remember to instantiate winch servo
@@ -53,15 +55,96 @@ void setup() {
   ColorSensor colorSensor = new ColorSensor();
 
   tEncoder.begin(pinIntakeEncoder1, pinIntakeEncoder2);
-  eMagnet.initialize(pinElecMag);
-  mDetector.initialize(pinMetDet);
+  eMagnet.initialize(pinElecMag); 
+
   lSwitch.initialize(pinLimSwitch);
   turntable.begin(tServo);
   
   Intake intake = new Intake(tEncoder, mDetector, lSwitch, eMagnet, turntable, colorSensor, colorServoPin);
+
+  int coinCount = 0;
+  int distMetalDetectToIntake;
+  int distIntakeToIRMatrix;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  drivetrain.drive(47, 0);
+  drivetrain.searchForward();
 
+  drivetrain.turnToAngle(90);
+  drivetrain.followLineUntilCoin();
+  if(intake.coinDetected())
+  {
+    drivetrain.drive(distMetalDetectToIntake, 0);
+    intake.pickUpSequenceA();
+    coinCount++;
+  }
+  drivetrain.drive(distIntakeToMatrix, 0);
+  drivetrain.turnToAngle(-135);
+  
+  while(coinCount != 4)
+  {
+    drivetrain.followLineUntilCoin();
+    if(intake.coinDetected())
+    {
+      drivetrain.drive(distMetalDetectToIntake, 0);
+      intake.pickUpSequenceA();
+      coinCount++;
+    }  
+  }
+  drivetrain.drive(distIntakeToMatrix, 0);
+  drivetrain.turnToAngle(-135);
+
+  while(coinCount != 6)
+  {
+    drivetrain.followLineUntilCoin();
+    if(intake.coinDetected())
+    {
+      drivetrain.drive(distMetalDetectToIntake, 0);
+      intake.pickUpSequenceA();
+      coinCount++;
+    }  
+  }
+  drivetrain.drive(distIntakeToMatrix, 0);
+  drivetrain.turnToAngle(-135);
+
+  while(coinCount != 9)
+  {
+    drivetrain.followLineUntilCoin();
+    if(intake.coinDetected())
+    {
+      drivetrain.drive(distMetalDetectToIntake, 0);
+      intake.pickUpSequenceA();
+      coinCount++;
+    }  
+  }
+  drivetrain.drive(distIntakeToMatrix, 0);
+  drivetrain.turnToAngle(135);
+
+  while(coinCount != 10)
+  {
+    drivetrain.followLineUntilCoin();
+    if(intake.coinDetected())
+    {
+      drivetrain.drive(distMetalDetectToIntake, 0);
+      intake.pickUpSequenceA();
+      coinCount++;
+    }  
+  }
+  drivetrain.drive(distIntakeToMatrix, 0);
+  drivetrain.turnToAngle(90);
+
+  while(coinCount != 12)
+  {
+    drivetrain.followLineUntilCoin();
+    if(intake.coinDetected())
+    {
+      drivetrain.drive(distMetalDetectToIntake, 0);
+      intake.pickUpSequenceA();
+      coinCount++;
+    }  
+  }
+
+  //drive till reach color and drive to all colors and go back to white square
 }

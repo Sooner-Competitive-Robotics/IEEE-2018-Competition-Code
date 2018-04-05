c7a7fedcb5b1daeb8fad86bcd9e7eda4d7512eaf
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2018-04-04 22:48:20 -0500

    Round 1 Uses all turnToAngle w/ steppers

--- Round1StepDrive/Round1StepDrive.ino
+++ Round1StepDrive/Round1StepDrive.ino
@@ -1,6 +1,8 @@
 #include <RobotLib.h>
 #include <IEEErobot2018.h>
 
+bool turnDone = false;
+
 void setup()
 {
   robotSetup();
@@ -10,32 +12,64 @@ void loop()
 {
 
 	// put your main code here, to run repeatedly:
+	
 	drivetrain.setRPM(DRIVE_RPM);
+	Serial.println(drivetrain.convertInchesToSteps(18*DIST_RATIO));
 	drivetrain.step(drivetrain.convertInchesToSteps(18*DIST_RATIO), drivetrain.convertInchesToSteps(18*DIST_RATIO));
 	commandTransition();
-
-	/////////////////////////////////
-	while (drivetrain.stepToAngle(90, yaw))
-	{
-		
-	}
-	resetGyro();
-	/////////////////////////////////
 	
+	/////////////////////////////////
+	//
+	drivetrain.setRPM(TURN_RPM);
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(90, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	Serial.println("Done");
+	turnDone = false;
+	resetGyro();
+	/*
 	drivetrain.setRPM(TURN_RPM);
 	//turn 90
 	drivetrain.step(drivetrain.convertInchesToSteps(-NINETY_DEG), drivetrain.convertInchesToSteps(NINETY_DEG));
 	commandTransition();
-
+	*/
+	//
+	/////////////////////////////////
+	
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(24*DIST_RATIO), drivetrain.convertInchesToSteps(24*DIST_RATIO));	
 	commandTransition();
   
+	/////////////////////////////////
+	//
+	drivetrain.setRPM(TURN_RPM);
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-135, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	
+	/*
 	drivetrain.setRPM(TURN_RPM);
 	//turn -135
 	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG));
 	delay(PICK_DELAY);
-  
+	*/
+	//
+	/////////////////////////////////
+	
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(17*DIST_RATIO), drivetrain.convertInchesToSteps(17*DIST_RATIO));
 	delay(PICK_DELAY);
@@ -48,19 +82,56 @@ void loop()
 	drivetrain.step(drivetrain.convertInchesToSteps(17*DIST_RATIO), drivetrain.convertInchesToSteps(17*DIST_RATIO));	  
 	delay(PICK_DELAY);
 
+	/////////////////////////////////
+	//
+	drivetrain.setRPM(TURN_RPM);
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-135, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	/*
 	drivetrain.setRPM(TURN_RPM);
 	//turn -135
 	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG)); 
 	commandTransition();
-
+	*/
+	//
+	/////////////////////////////////
+	
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(24*DIST_RATIO), drivetrain.convertInchesToSteps(24*DIST_RATIO));	  
 	delay(PICK_DELAY);
-  
+	
+	/////////////////////////////////
+	//
 	drivetrain.setRPM(TURN_RPM);
-	//turn -90
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-90, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	Serial.println("Done");
+	turnDone = false;
+	resetGyro();
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn 90
 	drivetrain.step(drivetrain.convertInchesToSteps(NINETY_DEG), drivetrain.convertInchesToSteps(-NINETY_DEG));
 	commandTransition();
+	*/
+	//
+	/////////////////////////////////
   
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(12*DIST_RATIO), drivetrain.convertInchesToSteps(12*DIST_RATIO));	  
@@ -74,19 +145,59 @@ void loop()
 	drivetrain.step(drivetrain.convertInchesToSteps(12*DIST_RATIO), drivetrain.convertInchesToSteps(12*DIST_RATIO));	
 	delay(PICK_DELAY);
    
+	/////////////////////////////////
+	//
+	pitchOffset = 0;
+	yaw = 0;
 	drivetrain.setRPM(TURN_RPM);
-	//turn -90
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-90, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	Serial.println("Done");
+	turnDone = false;
+	resetGyro();
+	
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn 90
 	drivetrain.step(drivetrain.convertInchesToSteps(NINETY_DEG), drivetrain.convertInchesToSteps(-NINETY_DEG));
 	commandTransition();
+	*/
+	//
+	/////////////////////////////////
   
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(24*DIST_RATIO), drivetrain.convertInchesToSteps(24*DIST_RATIO));
 	delay(PICK_DELAY);
   
+	/////////////////////////////////
+	//
+	drivetrain.setRPM(TURN_RPM);
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-135, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	/*
 	drivetrain.setRPM(TURN_RPM);
 	//turn -135
-	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG));
+	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG)); 
 	commandTransition();
+	*/
+	//
+	/////////////////////////////////
   
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(17*DIST_RATIO), drivetrain.convertInchesToSteps(17*DIST_RATIO));	
@@ -101,18 +212,55 @@ void loop()
 	delay(PICK_DELAY);
   
 	//Do a 180 and start drop off portion
+	/////////////////////////////////
+	//
 	drivetrain.setRPM(TURN_RPM);
-	drivetrain.step(drivetrain.convertInchesToSteps(NINETY_DEG), drivetrain.convertInchesToSteps(-NINETY_DEG));
-	drivetrain.step(drivetrain.convertInchesToSteps(NINETY_DEG), drivetrain.convertInchesToSteps(-NINETY_DEG));
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(180, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn 180
+	drivetrain.step(drivetrain.convertInchesToSteps(ONE_EIGHTY), drivetrain.convertInchesToSteps(ONE_EIGHTY)); 
 	commandTransition();
+	*/
+	//
+	/////////////////////////////////
   
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(-19*DIST_RATIO), drivetrain.convertInchesToSteps(-19*DIST_RATIO));	
 	commandTransition();  
   
+	/////////////////////////////////
+	//
 	drivetrain.setRPM(TURN_RPM);
-	drivetrain.step(drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG));
-	commandTransition();    
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-135, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn -135
+	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG)); 
+	commandTransition();
+	*/
+	//
+	/////////////////////////////////   
 	
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(-36*DIST_RATIO), drivetrain.convertInchesToSteps(-40*DIST_RATIO));	
@@ -126,34 +274,94 @@ void loop()
 	drivetrain.step(drivetrain.convertInchesToSteps(-2*DIST_RATIO), drivetrain.convertInchesToSteps(-2*DIST_RATIO));
 	commandTransition();   
   
+	/////////////////////////////////
+	//
 	drivetrain.setRPM(TURN_RPM);
-	drivetrain.step(drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG));
-	commandTransition();  
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-135, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn -135
+	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG)); 
+	commandTransition();
+	*/
+	//
+	///////////////////////////////// 
 	  
 	drivetrain.setRPM(DRIVE_RPM);  
 	drivetrain.step(drivetrain.convertInchesToSteps(-108*DIST_RATIO), drivetrain.convertInchesToSteps(-108*DIST_RATIO));
 	commandTransition();  
 
+	/////////////////////////////////
+	//
 	drivetrain.setRPM(TURN_RPM);
-	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG));
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-135, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	turnDone = false;
+	resetGyro();
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn -135
+	drivetrain.step(drivetrain.convertInchesToSteps(ONE_THIRTY_FIVE_DEG), drivetrain.convertInchesToSteps(-ONE_THIRTY_FIVE_DEG)); 
 	commandTransition();
+	*/
+	//
+	/////////////////////////////////
   
 	drivetrain.setRPM(DRIVE_RPM);
-	drivetrain.step(drivetrain.convertInchesToSteps(-36*DIST_RATIO), drivetrain.convertInchesToSteps(-42*DIST_RATIO));	
+	drivetrain.step(drivetrain.convertInchesToSteps(-42*DIST_RATIO), drivetrain.convertInchesToSteps(-42*DIST_RATIO));	
 	commandTransition();
   
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(-42*DIST_RATIO), drivetrain.convertInchesToSteps(-42*DIST_RATIO));	
 	commandTransition();
   
+	/////////////////////////////////
+	//
 	drivetrain.setRPM(TURN_RPM);
+	while (!turnDone)
+	{
+		updateGyro();
+		turnDone = drivetrain.stepToAngle(-90, yaw);
+		Serial.print("YAW: ");
+		Serial.println(yaw);
+		Serial.print("YAW OFFSET: ");
+		Serial.println(yawOffset);
+	}
+	Serial.println("Done");
+	turnDone = false;
+	resetGyro();
+	/*
+	drivetrain.setRPM(TURN_RPM);
+	//turn 90
 	drivetrain.step(drivetrain.convertInchesToSteps(NINETY_DEG), drivetrain.convertInchesToSteps(-NINETY_DEG));
 	commandTransition();
+	*/
+	//
+	/////////////////////////////////
   
 	drivetrain.setRPM(DRIVE_RPM);
 	drivetrain.step(drivetrain.convertInchesToSteps(-40*DIST_RATIO), drivetrain.convertInchesToSteps(-40*DIST_RATIO));	
 	commandTransition();
-  
+	
 	//TODO: implement dropoffsequence
 	while (true){}
+	
+	delay(2000);
 }
